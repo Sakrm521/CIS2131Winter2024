66fe8549baf9c8c43e88b97570337019c5980303
Sakrm521 <156106666+Sakrm521@users.noreply.github.com>
2024-01-14 18:42:24 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,3 @@
+print('My name is Sarah and I am taking this class as part of the Data Science Certificate Program')
+print('Right now I work for Wayne State as a Cancer Research Assistant and I am currently trying to apply to PhD programs')
+print('When I am not working I like to watch TV (currently re-watching Supernatural lol), paint or play games (either board games or videogames).')
